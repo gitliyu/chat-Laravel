0b3d31fed8a979c34f31d21c9b4d3d50cef22aa0
gitliyu <1042625709@qq.com>
2018-03-02 14:05:20 +0800

    webpack and gitignore opmitize

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,24 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+var webpackConfig = {
+  output: {
+    publicPath: '/',                              
+    chunkFilename: 'assets/[name].[chunkhash].js' 
+  },
+  resolve: {
+    alias: {
+      'resources': path.resolve(__dirname, 'resources'),
+      'Modules': path.resolve(__dirname, 'Modules'),
+    }
+  }
+}
+
+mix.webpackConfig(webpackConfig)
+mix.options({
+  extractVueStyles: true, // Extract .vue component styling to file, rather than inline.
+})
 
 mix.js('resources/assets/js/main.js', 'public/js')
-   .extract(['vue', "vue-router", "axios"])
+   .extract(['vue', "vuex", "element-ui","vue-router", "axios"])
    .sass('resources/assets/sass/app.scss', 'public/css');
